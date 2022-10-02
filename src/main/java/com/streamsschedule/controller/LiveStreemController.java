02a796483e36be5540f4c30555fe059be3d29341
MoMutawe1 <mutalogix@gmail.com>
2022-10-01 22:29:21 -0500

    implement the controller class with the http methods

--- src/main/java/com/streamsschedule/controller/LiveStreemController.java
+++ src/main/java/com/streamsschedule/controller/LiveStreemController.java
@@ -1,10 +1,51 @@
 package com.streamsschedule.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.streamsschedule.exception.LiveStreamNotFoundException;
+import com.streamsschedule.model.LiveStream;
+import com.streamsschedule.repository.LiveStreamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/streams")
 public class LiveStreemController {
 
+    private final LiveStreamRepository repository;
+
+    @Autowired
+    public LiveStreemController(LiveStreamRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<LiveStream> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id) throws LiveStreamNotFoundException {
+        return repository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@Valid @RequestBody LiveStream stream) {
+        return repository.create(stream);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody LiveStream stream, @PathVariable String id) {
+        repository.update(stream,id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repository.delete(id);
+    }
 }
